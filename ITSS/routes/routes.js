2df9efd50e4b3326262256e15cc28db7fb38fb43
Nduduzo Khanyile <nduduzo1@live.com>
2017-10-30 23:38:01 +0200

    Initial Commit to Master

---
+++ ITSS/routes/routes.js
@@ -0,0 +1,23 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+         .when('/', {
+             templateUrl: 'pages/account/login.html',
+             controller: 'loginController'
+         })
+    .when('/admin-dashboard', {
+        templateUrl: 'pages/admin/admin-dashboard.html',
+        controller: 'loginController'
+    })
+    .when('/teacher-dashboard', {
+        templateUrl: 'pages/teacher/teacher-dashboard.html',
+        controller: 'loginController'
+    })
+    .when('/bursar-dashboard', {
+        templateUrl: 'pages/bursar/bursar-dashboard.html',
+        controller: 'loginController'
+    })
+    .when('/student-dashboard', {
+        templateUrl: 'pages/student/student-dashboard.html',
+        controller: 'loginController'
+    });
+});
